c438d575ee4252ba0b1226dfefb62678a7452ba2
morevishal99 <107456969+morevishal99@users.noreply.github.com>
2022-07-24 01:37:01 +0530

    Add files via upload

---
+++ mens.js
@@ -0,0 +1,37 @@
+let menarr = JSON.parse(localStorage.getItem("allproducts")) || []
+mensData.forEach(function (elem) {
+    let div = document.createElement("div")
+
+    let img = document.createElement("img");
+    img.setAttribute("src", elem.image_url);
+    img.onclick =function() {
+        localStorage.setItem("Single_products", JSON.stringify(elem));
+        window.location.href = "proddesc.html"
+    }
+
+
+    let name = document.createElement("h5");
+    name.innerText = elem.name;
+
+    let price = document.createElement("p");
+    price.innerText = "Rs" + elem.price + "/";
+
+    let off = document.createElement("p");
+    off.innerText = "Off% Rs" + elem.strikedoffprice
+
+    let btn = document.createElement("button");
+    btn.addEventListener("click", function () {
+        addtocart(elem)
+    })
+    btn.innerText = "Add to Bag"
+
+    div.append(img, name, price, off, btn)
+    document.querySelector("#container").append(div)
+
+})
+
+function addtocart(elem) {
+    menarr.push(elem)
+    localStorage.setItem("allproducts", JSON.stringify(menarr))
+
+}
---
+++ womens.js
@@ -0,0 +1,40 @@
+    
+    
+   
+    
+
+
+
+    let womenarr = JSON.parse(localStorage.getItem("allproducts"))||[]
+  womensData.forEach(function (elem) {
+  
+    let div = document.createElement("div")
+
+    let img = document.createElement("img");
+    img.setAttribute("src", elem.image_url);
+
+    let name = document.createElement("h5");
+    name.innerText = elem.name;
+
+    let price = document.createElement("p");
+    price.innerText = "Rs" + elem.price + "/";
+
+    let off = document.createElement("p");
+    off.innerText = "Off% Rs" + elem.strikedoffprice
+
+    let btn = document.createElement("button");
+    btn.addEventListener("click", function () {
+      addtocart(elem)
+    })
+    btn.innerText = "Add to Bag"
+
+    div.append(img, name, price, off, btn)
+    document.querySelector("#container").append(div)
+  })
+
+  
+  function addtocart(elem) {
+    womenarr.push(elem)
+    localStorage.setItem("allproducts", JSON.stringify(womenarr))
+
+  }
